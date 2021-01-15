b577087a50f01dc69295981ae413209c025e068f
hamed <h.fakouri@yahoo.com>
2021-01-15 21:58:15 +0330

    asd

--- Src/Work.Web/Startup.cs
+++ Src/Work.Web/Startup.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Work.Web.Models;
 
 namespace Work.Web
 {
@@ -25,6 +27,7 @@ namespace Work.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<RostDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
